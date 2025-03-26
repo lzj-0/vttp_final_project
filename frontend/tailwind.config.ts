a64c270baa3a9b20e5041ee34106a65131f975fe
lzj-0 <jay260399@gmail.com>
2025-03-27 00:23:32 +0800

    updated readme for frontend

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default <Config>{
-  content: [], // Disable purging by setting content to an empty array
+  content: [],
   theme: {
     extend: {},
   },
